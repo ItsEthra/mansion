325861fcbc149e47f71b1e7b3dab31d5dc31ca30
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-06-23 12:29:18 +0300

    Simple adapter

---
+++ src/adapter/mod.rs
@@ -0,0 +1,14 @@
+mod simple;
+pub use simple::*;
+
+use crate::{Error, MessageType, SendSync};
+use cursored::Cursored;
+
+pub trait Adapter: SendSync {
+    type Message: MessageType;
+
+    fn cloned(&self) -> Box<dyn Adapter<Message = Self::Message>>;
+
+    fn encode(&self, msg: Self::Message, buf: &mut Cursored) -> Result<(), Error>;
+    fn decode(&self, buf: &mut Cursored) -> Result<Self::Message, Error>;
+}
---
+++ src/adapter/simple.rs
@@ -0,0 +1,23 @@
+use crate::{MessageType, Adapter};
+use std::marker::PhantomData;
+use cursored::Cursored;
+
+pub struct SimpleAdapter<M: MessageType>(PhantomData<M>);
+impl<M: MessageType> Adapter for SimpleAdapter<M> {
+    type Message = M;
+
+    fn cloned(&self) -> Box<dyn Adapter<Message = Self::Message>> {
+        Box::new(SimpleAdapter(PhantomData))
+    }
+
+    fn encode(&self, msg: Self::Message, buf: &mut Cursored) -> Result<(), crate::Error> {
+        let data = bincode::serialize(&msg)?;
+        buf.put_slice(&data[..]);
+
+        Ok(())
+    }
+
+    fn decode(&self, buf: &mut Cursored) -> Result<Self::Message, crate::Error> {
+        Ok(bincode::deserialize::<M>(buf.lasting())?)
+    }
+}
